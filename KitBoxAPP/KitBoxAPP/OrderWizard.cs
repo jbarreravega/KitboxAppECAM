304b572f7710f3295ebf754c9d553c8054185411
jbarreravega <jbarreravega@users.noreply.github.com>
2016-03-02 15:15:28 +0100

    Piece: PriceSupplier & Color changés. Color: Code & Name changés

---
+++ KitBoxAPP/KitBoxAPP/OrderWizard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBoxAPP
+{
+    class OrderWizard
+    {
+        private Furniture furniture;
+        private string name_client, address_client;
+
+        public OrderWizard(Furniture furniture, string name_client,
+                           string address_client)
+        {
+            this.furniture = furniture;
+            this.name_client = name_client;
+            this.address_client = address_client;
+        }
+
+        public Order CreateOrder()
+        {
+            return Nullable;
+        }
+    }
+}
