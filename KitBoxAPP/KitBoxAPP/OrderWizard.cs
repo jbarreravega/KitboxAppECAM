b27c1830f336ab142801c5791b908c14fa9ef425
Rémy Taymans <remytaymans@gmail.com>
2016-03-29 15:21:54 +0200

    Contient une liste de Furniture et non une seule Furniture.

--- KitBoxAPP/KitBoxAPP/OrderWizard.cs
+++ KitBoxAPP/KitBoxAPP/OrderWizard.cs
@@ -8,16 +8,23 @@ namespace KitBoxAPP
 {
     public class OrderWizard
     {
-        private Furniture furniture;
+        private List<Furniture> furniture;
         private string name_client, address_client;
 
-        public OrderWizard(Furniture furniture, string name_client,
+        public OrderWizard(List<Furniture> furniture, string name_client,
                            string address_client)
         {
             this.furniture = furniture;
             this.name_client = name_client;
             this.address_client = address_client;
         }
+
+		public OrderWizard()
+		{
+			this.furniture = new List<Furniture> ();
+			this.name_client = "";
+			this.address_client = "";
+		}
 
         public Order CreateOrder()
         {
